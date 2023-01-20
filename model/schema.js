bd2ff2e2e8fda8230c2e8979e64843bb84cf791e
Ganesh-devie <ganeshviswanathan66@gmail.com>
2023-01-20 10:04:38 +0530

    rds db

--- model/schema.js
+++ model/schema.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
+    host: "database-1.ccmdzhfdnifr.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "cloud9db",
     port: 3306,
     database:"firstdb"
   })
